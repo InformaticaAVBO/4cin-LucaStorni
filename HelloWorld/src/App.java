3af9e82064caf2add21121d72109af531b4594cd
LucaStorni <luca.storni@aldini.istruzioneer.it>
2025-10-24 07:43:27 +0000

    prova di scrittura seguita da lettura

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+     FileWriter myWriter = new FileWriter (filename:"filename.txt");
+
+     for(int i=0; i < 100; i++){
+        
+        myWriter.Writer ("Questa è una prima frase scritta in un file");
+     }
+     myWriter.close();
+
+     System.out.println("Ok ho scritto sul file!");
     }
 }
